a8675698930c8e08f4c140a1e2d1441c12944d8c
CJMoye <Cjmoye30@gmail.com>
2023-02-11 12:44:01 -0500

    Added skeleton for quiz questions and answer choices

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// Adding Event Listener for Start button
+// All Document Selectors
 var startBtn = document.querySelector("#start-button");
 var intro = document.querySelector("#intro");
+var questionSection = document.querySelector("#quiz-content");
+
+// All Event Listeners
 startBtn.addEventListener("click", removeIntro);
 
-// Remove intro when clicked
+// Remove the intro section when clicked and show the quiz content section
 function removeIntro (){
-    intro.setAttribute("style", "display: none;")
+    intro.setAttribute("style", "display: none;");
+    questionSection.setAttribute("style", "display: block;");
 }
