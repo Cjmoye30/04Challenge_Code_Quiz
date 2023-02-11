b0f73774d586cfc1777f38260daddf47bc220034
CJMoye <Cjmoye30@gmail.com>
2023-02-11 12:33:06 -0500

    Added HTML CSS JS for intro section - this is the greeting and text when the browswer opens up

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("Connected!")
+// Adding Event Listener for Start button
+var startBtn = document.querySelector("#start-button");
+var intro = document.querySelector("#intro");
+startBtn.addEventListener("click", removeIntro);
+
+// Remove intro when clicked
+function removeIntro (){
+    intro.setAttribute("style", "display: none;")
+}
