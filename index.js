793bac4055882d3dc8c33124f7277a44ec3b343a
CJMoye <Cjmoye30@gmail.com>
2023-02-13 13:19:43 -0500

    Added in new function called answer check which is working correctly

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-// All Document Selectors
+// All Document Selectors and Event Listeners
 var startBtn = document.querySelector("#start-button");
 var intro = document.querySelector("#intro");
 var questionSection = document.querySelector("#quiz-content");
-
-// Document Selectors for questions and answers
+var currentQ = document.querySelector("#question");
 var answerOptions = document.querySelectorAll(".answer-choices");
-for (var i = 0; i <answerOptions.length; i++){
-    answerOptions[i].addEventListener("click", function(event){
-        console.log(event.target.classList[1]);
-    })
-}
+var currentQResult = document.querySelector(".question-result");
 
-// All Event Listeners
-startBtn.addEventListener("click", removeIntro);
+// Initialize Counters
+var correct = 0;
+var wrong = 0;
+var qIndex = 1;
+
+// Query Selector for all answer chocies
+
+
+
+// Starting the quiz - I can put this in another function called execute whic
+startBtn.addEventListener("click", function () {
+    removeIntro();
+    returnQuestions();
+    runQuiz();
+    answerCheck();
+});
 
 // Remove the intro section when clicked and show the quiz content section
 function removeIntro() {
@@ -20,17 +29,16 @@ function removeIntro() {
     questionSection.setAttribute("style", "display: block;");
 }
 
-// Create the question bank
-
+// Question Bank
 function returnQuestions() {
 
     var q1 = {
         question: "What color is my underwear?",
         answers: {
-            a: "Red",
-            b: "Blue",
-            c: "Green",
-            d: "Potato"
+            0: "Red",
+            1: "Blue",
+            2: "Green",
+            3: "Potato"
         },
         correctAnswer: 'd'
     }
@@ -38,10 +46,10 @@ function returnQuestions() {
     var q2 = {
         question: "Who do you think you are?",
         answers: {
-            a: "Excuse me?",
-            b: "I am Cambric",
-            c: "A tame salmon",
-            d: "2+2=5"
+            0: "Excuse me?",
+            1: "I am Cambric",
+            2: "A tame salmon",
+            3: "2+2=5"
         },
         correctAnswer: 'b'
     }
@@ -49,27 +57,42 @@ function returnQuestions() {
     var q3 = {
         question: "What gives you the right?",
         answers: {
-            a: "My entire existence",
-            b: "null",
-            c: "Shovel!",
-            d: "I give up"
+            0: "My entire existence",
+            1: "null",
+            2: "Shovel!",
+            3: "I give up"
         },
         correctAnswer: 'a'
     }
 
     var qArray = [q1, q2, q3];
-    console.log(qArray);
     return qArray;
-
 }
 
-// Testing
-// // console.log(q1.answers.a);
-// var selection = q1.answers.a;
-// if(selection === q1.correctAnswer) {
-//     console.log("Correct!")
-// } else {
-//     console.log("Incorrect!")
-// }
+function runQuiz (){
+    currentQ.innerHTML = returnQuestions()[qIndex].question;
+    for(var i = 0; i <answerOptions.length; i++){
+        answerOptions[i].innerHTML = returnQuestions()[qIndex].answers[i];
+    }
+}
 
-returnQuestions();
+function answerCheck (){
+
+    var correct = returnQuestions()[qIndex].correctAnswer;
+    
+    // For loop to place an event listener on all of the answer choices
+    for (var i = 0; i < answerOptions.length; i++) {
+        answerOptions[i].addEventListener("click", function (event) {
+            console.log(event.target.classList[1]);
+
+            if(event.target.classList[1] === correct) {
+                console.log("Correct!");
+            } else {
+                console.log("Wrong!")
+            }
+        })
+    }
+
+    // Texting the answer choice against the correct answer.
+
+}
